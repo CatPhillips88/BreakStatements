1a1a04efd1d22169dc5c764a804123e00331865c
Catherine <catherineep4189@gmail.com>
2020-11-11 17:28:15 +0000

    format changes

--- Testing-Statement1.py
+++ Testing-Statement1.py
@@ -1,7 +1,7 @@
 places = ['Tokyo', 'Jamaica', 'Santorini', 'Scotland']
 
 for place in places:
-  new_place = input('Where to next? ')
+  new_place = input('WHERE TO NEXT? ')
 
   if new_place == 'quit':
     print('MY LIST:') 
@@ -9,7 +9,7 @@ for place in places:
       print(final_list_item)
     break
   elif new_place in places:
-    print('THIS DESTNATION IS ALREADY IN YOUR LIST! ')
+    print('THIS DESTINATION IS ALREADY IN YOUR LIST! ')
   else:
     places.append(new_place)
     for new_list_item in places:
